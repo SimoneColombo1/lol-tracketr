aee28bf4a576778622c00e50988aafb54fcd7eb4
SimoneColombo1 <simcolombo2@gmail.com>
2025-10-06 16:03:25 +0200

    Creata la Tabella games

---
+++ database/migrations/2025_10_06_135643_create_games_table.php
@@ -0,0 +1,30 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('games', function (Blueprint $table) {
+            $table->id();
+            $table->boolean("Risultato");
+            $table->date("data");
+            $table-> string("ruolo");
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('games');
+    }
+};
